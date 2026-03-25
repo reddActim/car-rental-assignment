bf24b3a1c1796b04f97b4af791c645051466fba0
reddActim <reddactim@gmail.com>
2026-03-25 22:52:43 +0530

    changes to db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,19 @@
 const mysql = require('mysql2');
-require('dotenv').config();  // load .env
 
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
 
-module.exports = pool.promise();
+connection.connect((err) => {
+  if (err) {
+    console.error('Database connection failed:', err.stack);
+    return;
+  }
+  console.log('Connected to database.');
+});
+
+module.exports = connection;
